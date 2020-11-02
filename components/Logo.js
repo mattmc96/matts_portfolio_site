44c7c12e3498f1ea6a1361128d39948d6526b4e7
Matthew Mccarthy <matthewmc96@gmail.com>
2020-11-01 23:35:29 -0600

    fixed elements done

--- components/Logo.js
+++ components/Logo.js
@@ -1,12 +1,31 @@
 import Link from 'next/link'
+import Typed from 'react-typed'
 
-const Logo = () => (
-        <div class="block flex sm:inline-block">
-            <Link href="/">
-                <a>
-                    <img src="/me.png" alt="me" class="h-auto w-auto"/>
-                </a>
-            </Link>
-        </div>
-)
-export default Logo
+export default function Logo() {
+    return (
+
+            <div class="flex flex-row-reverse justify-center items-center gap-x-4  ">
+
+                    <Link href="/">
+                        <a>
+                            <img src="/me.png" alt="me" class="h-56 w-56"/>
+                        </a>
+                    </Link>
+                    <div class="absolute left-0 ml-40 ">
+                        <Typed
+                                strings={[
+                                    'Hi, my names Matt!',
+                                    'I\'m a full-stack web developer',
+                                    'Welcome to my portfolio',
+                                    'I\'m currently available for hire'
+                                ]}
+                                typeSpeed={60}
+                                backSpeed={60}
+                                smartBackspace={true}
+                                loop={false}/>
+
+                    </div>
+            </div>
+    )
+}
+
