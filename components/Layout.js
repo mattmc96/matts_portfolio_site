914fb1eafed79a5b8f13c31549cecdd6d2d4993c
Matthew Mccarthy <matthewmc96@gmail.com>
2020-11-10 16:13:55 -0600

    host

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,18 @@
-import Header from "../components/Header";
-import Footer from "../components/Footer";
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import Head from 'next/head'
 
 const Layout = (props) => (
-        <div>
-            <Header />
-            <div class="sm:min-h-0 text-center font-Roboto">{props.children}</div>
-            <Footer />
-        </div>
-);
-export default Layout;
+        <>
+            <Head>
+                <title>Matts Porfolio</title>
+                <meta name="image" property="og:image" content="/screenshotportfolio.png"/>
+            </Head>
+            <div>
+                <Header/>
+                <div class="sm:min-h-0 text-center font-Roboto">{props.children}</div>
+                <Footer/>
+            </div>
+        </>
+)
+export default Layout
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,9 @@
 import '../styles/tailwind.css'
 import '../styles/global.css'
 
+
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return <Component {...pageProps}  />
 }
 
 export default MyApp
